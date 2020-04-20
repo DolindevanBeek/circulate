d64d3f7fd25531f123d01af5b413ce23cebe60c2
DolindevanBeek <dolindevanbeek@gmail.com>
2020-04-20 22:22:44 +0200

    changed to chat app

--- server.js
+++ server.js
@@ -8,13 +8,7 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// var app = require('express')();
-// var http = require('http').createServer(app);
 const io = socketIO(server);
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -30,7 +24,3 @@ io.on('connection', (socket) => {
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-// http.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
