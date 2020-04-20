e875775a2cf42983c54e73dd7497565d811013e5
DolindevanBeek <dolindevanbeek@gmail.com>
2020-04-20 22:17:37 +0200

    time

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// var app = require('express')();
+// var http = require('http').createServer(app);
+const io = socketIO(server);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+// http.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
